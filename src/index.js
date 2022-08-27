74471a1080551c1eb98ddb1cb4468827e2974419
Jeff <odhiambo47@gmail.com>
2022-08-28 02:04:57 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const input = document.querySelector('input#serchByID')
+
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4')
+            const summary = document.querySelector('section#movieDetails p')
+
+            title.innertText = data.title
+            summary.innertText = data.summary
+        })
+    
+    }) 
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
+
